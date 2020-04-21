5b86394a26664a82e37dff48bcc642aab0bbbf25
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-04-21 12:53:40 -0700

    Added functional countdown timer starting from 10 seconds on page load.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,21 @@
 var button = document.querySelector("#btnStart");
+var counter = document.querySelector("#counter");
+button.addEventListener("click", function () { });
+
+var totalTime = 10
+
+var timer = setInterval(startTimer, 1000);
 
 
+function startTimer() {
+    totalTime--;
+    counter.innerHTML = totalTime
 
-button.addEventListener("click", function () { })
+    if (totalTime === 0) {
+        clearInterval(timer)
+        counter.parentElement.innerHTML = "Time's Up!"
+    }
+};
+
+
+var timeHead = document.body.firstElementChild.childNodes[1].childNodes[3]
