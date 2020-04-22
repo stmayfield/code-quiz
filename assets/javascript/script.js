4cb47852b95d471afd139dc7de9c29dc5aefcb23
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-04-21 23:13:21 -0700

    Replaced button element with custom .svg img and blank href. Added alternate appearance on mouseover even in script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,7 @@
-var button = document.querySelector("#btnStart");
+var button01 = document.querySelector("#btnStart");
 var counter = document.querySelector("#counter");
-button.addEventListener("click", function () { });
+var mouseOver = document.querySelector(".custom-button");
+button01.addEventListener("click", function () { });
 var totalTime = 10
 
 var timer = setInterval(startTimer, 1000);
@@ -17,6 +18,15 @@ function startTimer() {
 };
 
 
-button.addEventListener("click", function () {
+button01.addEventListener("click", function () {
     alert("Test");
 });
+
+button01.addEventListener("mouseover", function () {
+    button01.setAttribute("src", "assets/Button01-02.svg");
+});
+button01.addEventListener("mouseout", function () {
+    button01.setAttribute("src", "assets/Button01-01.svg");
+});
+
+document
