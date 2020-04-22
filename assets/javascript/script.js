ee1e77c5986ca331aa3380ebfb5b65fbb429f507
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-04-21 23:45:57 -0700

    replaced test alert with 10 second countdown timer

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,26 +2,16 @@ var button01 = document.querySelector("#btnStart");
 var counter = document.querySelector("#counter");
 var mouseOver = document.querySelector(".custom-button");
 button01.addEventListener("click", function () { });
-var totalTime = 10
-
-var timer = setInterval(startTimer, 1000);
+var totalTime = 11
 
 
-function startTimer() {
-    totalTime--;
-    counter.innerHTML = totalTime
 
-    if (totalTime === 0) {
-        clearInterval(timer)
-        counter.parentElement.innerHTML = "Time's Up!"
-    }
-};
+
 
 
 button01.addEventListener("click", function () {
-    alert("Test");
+    setInterval(startTimer, 1000);
 });
-
 button01.addEventListener("mouseover", function () {
     button01.setAttribute("src", "assets/Button01-02.svg");
 });
@@ -29,4 +19,12 @@ button01.addEventListener("mouseout", function () {
     button01.setAttribute("src", "assets/Button01-01.svg");
 });
 
-document
+function startTimer() {
+    totalTime--;
+    counter.innerHTML = totalTime
+
+    if (totalTime === 0) {
+        clearInterval(setInterval(startTimer, 1000))
+        counter.parentElement.innerHTML = "Time's Up!"
+    }
+};
