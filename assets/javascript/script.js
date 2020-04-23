f46c36c59cf3fb3023ef19b8960f4e5b226003f5
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-04-22 22:02:20 -0700

    Added placeholder elements to index.html for quiz block. Assigned assigned elements to placeholder variables. Replaced first <p> html element with DOM methods in script.js. Replaced first answer 'Replacement Text 1' using replaceChild method with 'Replacement Text 5'.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,23 +1,18 @@
 var button01 = document.querySelector("#btnStart");
 var counter = document.querySelector("#counter");
 var mouseOver = document.querySelector(".custom-button");
-button01.addEventListener("click", function () { });
-var totalTime = 11
+var countMessage = counter.parentElement;
+var totalTime = 6
 
 
 
 
-
-
-button01.addEventListener("click", function () {
-    setInterval(startTimer, 1000);
-});
-button01.addEventListener("mouseover", function () {
-    button01.setAttribute("src", "assets/Button01-02.svg");
-});
-button01.addEventListener("mouseout", function () {
-    button01.setAttribute("src", "assets/Button01-01.svg");
-});
+function activateTimer() {
+    button01.addEventListener("click", function timer() {
+        setInterval(startTimer, 1000);
+        // startQuiz();
+    });
+};
 
 function startTimer() {
     totalTime--;
@@ -25,6 +20,66 @@ function startTimer() {
 
     if (totalTime === 0) {
         clearInterval(setInterval(startTimer, 1000))
-        counter.parentElement.innerHTML = "Time's Up!"
+        countMessage.innerHTML = "Time's Up!"
     }
 };
+
+activateTimer();
+
+// function startQuiz() {
+//     block
+// };
+
+var quizBlock = document.querySelector(".div2");
+// var answer1 = document.querySelector("#answer-one");
+
+var ans1 = document.createElement("p")
+ans1.setAttribute("id", "answer-one")
+ans1.textContent = "Replacement text 1";
+quizBlock.appendChild(ans1)
+
+var ans5 = document.createElement("p")
+ans5.setAttribute("id", "answer-five")
+ans5.textContent = "Replacement text 5";
+quizBlock.appendChild(ans5)
+
+var answer2 = document.querySelector("#answer-two");
+var answer3 = document.querySelector("#answer-three");
+var answer4 = document.querySelector("#answer-four");
+
+
+
+quizBlock.hidden = false;
+// answer1.textContent = "[LEGACY]Replacement text 1";
+answer2.textContent = "Replacement text 2";
+answer3.textContent = "Replacement text 3";
+answer4.textContent = "Replacement text 4";
+
+
+ans1.addEventListener("click", function () {
+    alert("incorrect");
+});
+answer2.addEventListener("click", function () {
+    alert("COreccet!");
+});
+answer3.addEventListener("click", function () {
+    alert("incorrect");
+});
+answer4.addEventListener("click", function () {
+    alert("incorrect");
+});
+
+
+
+quizBlock.replaceChild(ans5, ans1);
+
+
+
+
+//Button animation
+button01.addEventListener("mouseover", function () {
+    button01.setAttribute("src", "assets/Button01-02.svg");
+});
+button01.addEventListener("mouseout", function () {
+    button01.setAttribute("src", "assets/Button01-01.svg");
+});
